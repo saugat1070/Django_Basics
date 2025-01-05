caac0e5b4a6a2615530b6efa9b1623b6ea3e4397
saugat1070 <saugatgiri1070@gmail.com>
2025-01-05 23:52:37 +0545

    pushing all file from github

--- Django_basics/FirstApp/forms.py
+++ Django_basics/FirstApp/forms.py
@@ -4,4 +4,11 @@ class StudentForm(forms.Form):
     stadd = forms.CharField()
     stno = forms.IntegerField()
     esal = forms.FloatField()
+
+    def cleanstname(self):
+        input_name = forms.clean_data['stname']
+        if len(input_name) < 3 and len(input_name) > 10:
+            raise ValidationError('Character of name shuold be grater than 3 and less than 10')
+        
+        return input_name
     
